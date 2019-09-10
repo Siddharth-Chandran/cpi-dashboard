a47647120ac58cf4da977b320d623062b5992ca2
Siddharth Chandran <siddharth.chandrane@gmail.com>
2019-09-10 13:17:28 +0530

    First commit with dashboard template

---
+++ src/components/dashboard/Dashboard.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import SportsSoccerOutlined from '@material-ui/icons/SportsSoccerOutlined';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+import PropTypes from 'prop-types';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://github.com/Siddharth-Chandran/cpi-dashboard">
+                Straw Hats
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const styles = theme => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+});
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+class Dashboard extends React.Component {
+    render() {
+
+
+        const { classes } = this.props;
+
+        return (
+            <React.Fragment>
+                <CssBaseline />
+                <AppBar position="relative">
+                    <Toolbar>
+                        <SportsSoccerOutlined className={classes.icon} />
+                        <Typography variant="h6" color="inherit" noWrap>
+                            CPI - StrawHats
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <main>
+                    {/* Hero unit */}
+                    <div className={classes.heroContent}>
+                        <Container maxWidth="sm">
+                            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                                CPI Prediction
+                            </Typography>
+                            <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                               A group of lawless pirates trying to predict the cpi index of India. 
+                            </Typography>
+                        </Container>
+                    </div>
+                    <Container className={classes.cardGrid} maxWidth="md">
+                        Need to put up graphs here
+                    </Container>
+                </main>
+                {/* Footer */}
+                <footer className={classes.footer}>
+                    <Typography variant="h6" align="center" gutterBottom>
+                        Monkey D Luffy
+        </Typography>
+                    <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+                        Gomu gomu ...!
+        </Typography>
+                    <Copyright />
+                </footer>
+                {/* End footer */}
+            </React.Fragment>
+        );
+    }
+}
+
+Dashboard.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Dashboard);
