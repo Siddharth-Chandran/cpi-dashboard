23b47518778caec9d4c36b7ad4fd12a6ab16b02c
Aman Goyal <amangoyal1412@gmail.com>
2019-09-13 13:54:48 +0530

    Improved graph

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -15,15 +15,30 @@ import Button from '@material-ui/core/Button';
 
 /** Needs to be removed after completing backend API */
 const data = [
-    { month: 'January', actual: 4000, predicted: 5500, },
-    { month: 'February', actual: 3000, predicted: 3500, },
-    { month: 'March', actual: 2000, predicted: 3000, },
-    { month: 'April', actual: 2780, predicted: 3908, },
-    { month: 'May', actual: 1890, predicted: 3000, },
-    { month: 'June', actual: 2390, predicted: 3800, },
-    { month: 'July', actual: 3490, predicted: 4300, },
-    { month: 'Auguest', actual: 3490, predicted: 4300, },
-    { month: 'September', predicted: 4300, },
+{'month': '2017-08-01', 'actual': 129.8, 'predicted': 129.45809104557068},
+{'month': '2017-09-01', 'actual': 131.0, 'predicted': 130.10434840701507},
+{'month': '2017-10-01', 'actual': 132.2, 'predicted': 131.69835331432338},
+{'month': '2017-11-01', 'actual': 135.3, 'predicted': 132.7570800400743},
+{'month': '2017-12-01', 'actual': 136.6, 'predicted': 136.78161568245736},
+{'month': '2018-01-01', 'actual': 136.6, 'predicted': 137.17576719906998},
+{'month': '2018-02-01', 'actual': 136.7, 'predicted': 136.57877863161767},
+{'month': '2018-03-01', 'actual': 136.5, 'predicted': 136.58011318951367},
+{'month': '2018-04-01', 'actual': 136.5, 'predicted': 136.14127745386108},
+{'month': '2018-05-01', 'actual': 136.9, 'predicted': 136.2531466498307},
+{'month': '2018-06-01', 'actual': 138.1, 'predicted': 138.15065128154185},
+{'month': '2018-07-01', 'actual': 139.7, 'predicted': 139.29279728178417},
+{'month': '2018-08-01', 'actual': 140.9, 'predicted': 140.72929358881444},
+{'month': '2018-09-01', 'actual': 142.3, 'predicted': 141.691090115265},
+{'month': '2018-10-01', 'actual': 143.5, 'predicted': 142.96817538683968},
+{'month': '2018-11-01', 'actual': 145.1, 'predicted': 143.92684655276898},
+{'month': '2018-12-01', 'actual': 142.7, 'predicted': 145.75087096050996},
+{'month': '2019-01-01', 'actual': 139.5, 'predicted': 142.09819529313359},
+{'month': '2019-02-01', 'actual': 138.4, 'predicted': 138.40226895094136},
+{'month': '2019-03-01', 'actual': 139.7, 'predicted': 138.81392328042222},
+{'month': '2019-04-01', 'actual': 140.0, 'predicted': 141.30510011820704},
+{'month': '2019-05-01', 'actual': 140.3, 'predicted': 140.85413362852753},
+{'month': '2019-06-01', 'actual': 141.2, 'predicted': 142.65377211532015},
+{'month': '2019-07-01', 'actual': 139.2, 'predicted': 142.92884259303975}
 ];
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -151,11 +166,11 @@ class Dashboard extends React.Component {
                                 top: 5, right: 30, left: 20, bottom: 5,
                             }}>
                             <CartesianGrid strokeDasharray="3 3" />
-                            <XAxis dataKey="name" />
-                            <YAxis />
+                            <XAxis dataKey="month" />
+                            <YAxis domain={[125, 148]} />
                             <Tooltip />
                             <Legend />
-                            <Line type="monotone" dataKey="actual" stroke="#8884d8" activeDot={{ r: 8 }} />
+                            <Line type="monotone" dataKey="actual" stroke="#ff0000" activeDot={{ r: 8 }} />
                             <Line type="monotone" dataKey="predicted" stroke="#82ca9d" />
                         </LineChart>
                     </Container>
